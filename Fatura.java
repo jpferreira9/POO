86dac3c657b609cec9395631a3467a07267934c5
MarkKopfner <28536327+MarkKopfner@users.noreply.github.com>
2018-05-01 11:15:25 +0100

    Chnge

--- Fatura.java
+++ Fatura.java
@@ -1,4 +1,84 @@
 import java.util.*;
+<<<<<<< HEAD
+import java.time.LocalDate;
+
+public class Fatura extends Empresa {
+ 
+    private LocalDate data;
+    private int nifCliente;
+    private String descriçao;
+    private double valor;
+    
+    public Fatura(){
+        super();
+        this.data = LocalDate.of(2018,1,1);
+        this.nifCliente = 0;
+        this.descriçao = "";
+        this.valor = 0.0;
+    }
+    
+    public Fatura(int nif,String email,String nome,String morada,String password,ArrayList<String> actividade,int ffiscalorFiscal,LocalDate data,int nifCliente,String descriçao,double valor){
+        super(nif,email, nome, morada, password,actividade,ffiscalorFiscal);
+        this.data = data;
+        this.nifCliente = nifCliente;
+        this.descriçao = descriçao;
+        this.valor = valor;
+    }
+    
+    public Fatura(Fatura f){
+        super(f);
+        this.data = f.getData();
+        this.nifCliente = f.getnifCliente();
+        this.descriçao = f.getDescriçao();
+        this.valor = f.getValor();
+    }
+    
+    public LocalDate getData(){
+        return this.data;
+    }
+    
+    public int getnifCliente(){
+        return this.nifCliente;
+    }
+    
+    public String getDescriçao(){
+        return this.descriçao;
+    }
+    
+    public double getValor(){
+        return this.valor;
+    }
+    
+    public void setData(LocalDate data){
+        this.data = data;
+    }
+    
+    public void setnifCliente(int nifCLiente){
+        this.nifCliente = nifCliente;
+    }
+    
+    public void setDescriçao(String descriçao){
+        this.descriçao = descriçao;
+    }
+    
+    public void imprime(){ //criar fatura de venda
+    }
+    
+    public void editar(){ //associar/editar classificação de actividade
+    }
+    
+    public void lista(){//obter a listagem das faturas da empresa, ordenada por data ou valor
+    }
+    
+    public void cliente(){//obter a listagem das faturas de um cliente, ordenada por data ou valor
+    }
+    
+    public void total(){//total facturado por uma empresa num determinado período
+    }
+
+    public Fatura clone(){
+        return new Fatura(this);
+=======
 import java.time.*;
 
 public class Fatura{
@@ -15,5 +95,6 @@ public class Fatura{
     }
     
     public void total(){//total facturado por uma empresa num determinado período
+>>>>>>> 80b5a65ca9d47f0f8869d8b03c7e21b9e133201f
     }
 }
--- Gestao.java
+++ Gestao.java
@@ -11,13 +11,25 @@ public class Gestao implements java.io.Serializable{
     Scanner in = new Scanner(System.in);
     
     HashMap<Integer,String> users =  new HashMap<Integer,String>();
+<<<<<<< HEAD
+    ArrayList<Integer> nifAgregado = new ArrayList<Integer>();
+=======
+>>>>>>> 80b5a65ca9d47f0f8869d8b03c7e21b9e133201f
     //HashMap<Integer,Individual> dadosI = new HashMap<Integer,Individual>();
     //HashMap<Integer,Empresa> dadosE = new HashMap<Integer, Empresa>();
     
     public void out(Object o){
         System.out.println(o.toString());
     }
+<<<<<<< HEAD
+    public boolean nifValido(int x){
+        int z = x/100000000;
+        if(x == 1234 || (z > 0.9 && z < 10 && (z < 3|| z >= 5 || z < 7 || z >= 8))) return true;
+        else return false;
+    }
+=======
     
+>>>>>>> 80b5a65ca9d47f0f8869d8b03c7e21b9e133201f
     public void admin(){
        while(true){ 
             menu.admin();
@@ -103,8 +115,11 @@ public class Gestao implements java.io.Serializable{
     
     public Gestao(){
             users.put(1234,"bolas");
+<<<<<<< HEAD
+=======
             users.put(123456789,"teste");
             users.put(987654321,"teste");
+>>>>>>> 80b5a65ca9d47f0f8869d8b03c7e21b9e133201f
             load();
             while(true){    
                 menu.login();                     
@@ -112,15 +127,24 @@ public class Gestao implements java.io.Serializable{
                     case 1: // Login de utilizador 
                         out("\nIntroduza NIF: ");
                         int a = in.nextInt();
+<<<<<<< HEAD
+                        if(nifValido(a)==true){
+=======
                         int z = a/100000000;
                         if(a == 1234 || (z > 0.9 && z < 10 && (z < 3|| z >= 5 || z < 7 || z >= 8))){
+>>>>>>> 80b5a65ca9d47f0f8869d8b03c7e21b9e133201f
                             if(users.containsKey(a) == true){
                                out("\nIntroduza password: ");
                                String pw = in.next();
                                if(users.get(a).equals(pw)){
                                    if(a == 1234) admin();
+<<<<<<< HEAD
+                                   else if(a < 300000000) individual(a);
+                                   else if(a > 300000000) empresa(a);
+=======
                                    else if(z < 3) individual(a);
                                    else if(z > 3) empresa(a);
+>>>>>>> 80b5a65ca9d47f0f8869d8b03c7e21b9e133201f
                                }
                                else {
                                    out("\nPassword incorrecta!");
@@ -140,10 +164,16 @@ public class Gestao implements java.io.Serializable{
                     case 2: // Registar Utilizador
                         out("\nIntroduza NIF: ");
                         a = in.nextInt();
+<<<<<<< HEAD
+                        if(nifValido(a)==true){
+                            boolean tipoI;
+                            if(a<300000000)tipoI = true;
+=======
                         z = a/100000000;
                         if(z > 0.9 && z < 10 && (z < 3|| z >= 5 || z < 7 || z >= 8)){
                             boolean tipoI;
                             if(z<3)tipoI = true;
+>>>>>>> 80b5a65ca9d47f0f8869d8b03c7e21b9e133201f
                             else tipoI = false;
                             boolean check = users.containsKey(a);
                             if(check == false) {
@@ -152,17 +182,46 @@ public class Gestao implements java.io.Serializable{
                                 users.put(a,pw);
                                 if(tipoI){ // DEFINIDO COMO INDIVIDUAL
                                     out("\nIntroduza o seu nome");
+<<<<<<< HEAD
+                                    String nome = in.nextLine();
+                                    out("\nIntroduza o seu e-mail");
+                                    String mail = in.nextLine();
+                                    out("\nIntroduza a sua morada");
+                                    String morada = in.nextLine();
+                                    out("\nIntroduza o numero de dependentes do agregado familiar");
+                                    int depAgreg = in.nextInt();
+                                    out("Introduza os NIFs do agregado familiar");
+                                    while(depAgreg > 0){
+                                        int y = in.nextInt();
+                                        if(nifValido(y)==true){
+                                            nifAgregado.add(y);
+                                            depAgreg--;
+                                        }
+                                        else out("NIF inválido");
+                                    }
+=======
                                     out("\nIntroduza o seu e-mail");
                                     out("\nIntroduza a sua morada");
                                     out("\nIntroduza o numero de dependentes do agregado familiar");
                                     out("Introduza os NIFs do agregado familiar");
+>>>>>>> 80b5a65ca9d47f0f8869d8b03c7e21b9e133201f
                                 }
                                 
                                 else { // DEFINIDO COMO EMPRESA
                                     out("\nIntroduza o nome da empresa");
+<<<<<<< HEAD
+                                    String nome = in.nextLine();
+                                    out("\nIntroduza o seu e-mail");
+                                    String mail = in.nextLine();
+                                    out("\nIntroduza a sua morada");
+                                    String morada = in.nextLine();
+                                    out("\nIntroduza as atividades economicas");
+                                    
+=======
                                     out("\nIntroduza o seu e-mail");
                                     out("\nIntroduza a sua morada");
                                     out("\nIntroduza as atividades economicas");
+>>>>>>> 80b5a65ca9d47f0f8869d8b03c7e21b9e133201f
                                 }
                                 out("\nRegisto efetuado");
                                 save(users);
@@ -211,10 +270,13 @@ public class Gestao implements java.io.Serializable{
             
             ois.close();
             fis.close();
+<<<<<<< HEAD
+=======
             /* PRINT HASHMAP GUARDADO
             for(Map.Entry<Integer,String> u : users.entrySet()){
                 out("Username: {"+u.getKey()+"} Password: {"+u.getValue()+"}\n");
             }*/
+>>>>>>> 80b5a65ca9d47f0f8869d8b03c7e21b9e133201f
         }
         catch(Exception e){}
     }
