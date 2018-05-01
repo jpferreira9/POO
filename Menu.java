3f6889732674191235cf9d94cf85cf2f3f284965
luisfelix15 <38686895+luisfelix15@users.noreply.github.com>
2018-05-01 11:04:32 +0100

    Add files via upload

--- Menu.java
+++ Menu.java
@@ -1,32 +1,44 @@
 import java.util.*;
 
-public class Menu{
+public class Menu {
+    Empresa emp = new Empresa();
+    Individual ind = new Individual();
+    //Entidade ent = new Entidade();
+    //Fatura fact = new Fatura();
+    
+    static HashMap<Integer, String> users = new HashMap<>();
+    
+    
     public static void clear() {  
         System.out.print("\u000C");  
-        System.out.flush();  
+        System.out.flush();
     }
-    public void out(Object o){
+    
+    public static void out(Object o){
         System.out.println(o.toString());
     }
-    public void login(){
+    
+    public static void login_menu(){
         clear();
         out("\n\t\tBem vindo ao JavaFactura\n\n\n\nEscolha a sua opção:");
         out("\n\t1)Fazer login"); 
-        //FEITO validar o acesso à aplicação utilizando as credenciais (nif e password)
+        //validar o acesso à aplicação utilizando as credenciais (nif e password)
         out("\n\t2)Registar um novo utilizador"); 
-        //FEITO registar um contribuinte, quer seja individual ou empresa
+        //registar um contribuinte, quer seja individual ou empresa
     }
-    public void individual(int nif){
+    
+    public static void menu_individual(){
         clear();
-        out("\n\t Bem vindo\n\n\n\nEscolha a sua opção:");
+        out("\n\t Bem vindo, escolha a sua opção: \n\n\n\n\n");
         out("\n\t1)Verificar despesas"); 
         //verificar, por parte do contribuinte individual, as despesas que foram emitidas em seu nome 
         out("\n\t2)Verificar dedução fiscal acumulada");
         // verificar o montante de dedução fiscal acumulado, por si e pelo agregado familiar;
     }
-    public void empresa(int nif){
+    
+    public static void menu_empresa(){
         clear();
-        out("\n\t Bem vindo\n\n\n\nEscolha a sua opção:");
+        out("\n\t Bem vindo, escolha a sua opção: \n\n\n\n\n");
         out("\n\t1)Criar fatura"); 
         //criar facturas associadas a despesas feitas por um contribuinte individual
         out("\n\t2)Editar fatura"); 
@@ -39,16 +51,137 @@ public class Menu{
          obter por parte das empresas, as listagens das facturas por contribuinte ordenadas por valor decrescente de despesa */
         out("\n\t5)Verificar total faturado"); 
         // indicar o total facturado por uma empresa num determinado período
+        
+        Scanner in = new Scanner(System.in);
+        switch(in.nextInt()){
+             case 1:
+                    
+                    
+        }
     }
-    public void admin(){
+    
+    public static void menu_admin(){
         clear();
-        out("\n\t Bem vindo ADMIN\n\n\n\nEscolha a sua opção:");
+        out("\n\t Bem vindo, escolha a sua opção: \n\n\n\n\n");
         out("\n\t1)Verificar 10 contribuintes que mais gastam");
         // determinar a relação dos 10 contribuintes que mais gastam em todo o sistema
         out("\n\t2)Verificar as X empresas com maior nº de faturas e as suas deduções fiscais");
         /* determinar a relação das X empresas que mais facturas em todo o sistema e o montante de deduções 
          fiscais que as despesas registadas (dessas empresas) representam*/
-        out("\n\t3)RESET");
         
     }
+    
+    public static void registar_empresa(int nif, String nome, String email, String morada, String pass){
+        clear();
+        Scanner in = new Scanner(System.in);
+        out("\nQuantas atividades a sua empresa esta associada? ");
+        int numact = in.nextInt();
+        ArrayList<String> activ = new ArrayList<String>(numact);
+        if ( numact > 0 ){
+            for (int i = 0; i<numact;i++){
+                out ("\nDigite umas das atividades");
+                activ.add(in.next());
+            }
+        }
+        Empresa emp = new Empresa(nif,nome,email,morada,pass,activ,1);
+    }
+    
+    public static void registar_individual(int nif, String nome, String email, String morada, String pass){
+        clear();
+        Scanner in = new Scanner(System.in);
+        out("\nPor quantos membros e formado o seu agregado familiar? ");
+        int agreg = in.nextInt();
+        ArrayList<Integer> nifAgreg = new ArrayList<Integer>(agreg);
+        if (agreg > 0 ){
+            for (int i = 0; i<agreg;i++){
+                out ("\n Introduza um NIF: "); 
+                nifAgreg.add(in.nextInt());
+                out ("\nbleble");
+            }
+        }
+        Individual ind = new Individual(nif,nome,email,morada,pass,agreg,nifAgreg,1,1);
+    }
+    
+    public static void save(){
+        //gravar o estado da aplicação em cheiro, para que seja possível retomar mais tarde a execução    
+    }
+    
+    public static void menu(){ 
+        users.put(1234, "bolas");
+        login_menu();
+        Scanner in = new Scanner(System.in);
+        
+        switch(in.nextInt()){
+            case 1: // Fazer Login de utilizador existente
+                out("\nIntroduza NIF: ");
+                int a = in.nextInt();
+                if(a == 1234 || (a > 99999999 && a < 1000000000 && (a < 300000000 || a >= 500000000 || a < 700000000 || a >= 800000000))){
+                    boolean check = users.containsKey(a);
+                    if(check == true){
+                       out("\nIntroduza password: ");
+                       String pw = in.next();
+                       if(pw.equals(users.get(a))){
+                           if(a == 1234) menu_admin();
+                           else if(a < 300000000) menu_individual();
+                           else if(a> 3000000) menu_empresa();
+                       }
+                       else {
+                           out("\nPassword incorrecta");
+                           break;
+                        }
+                    }
+                    else {
+                        out("\nNIF não existe");
+                        break;
+                    }        
+                }
+                else {
+                    out("\nNIF inválido");
+                    break;
+                }
+                            
+            case 2: // Registar Utilizador
+                out("\nIntroduza NIF: ");
+                int b = in.nextInt();
+                if(b > 99999999 && b < 1000000000 && (b < 300000000 || b >= 500000000 || b < 700000000 || b >= 800000000)){
+                    boolean check = users.containsKey(b);
+                    if(check == false) {
+                        
+                        out("\nPassword:");
+                        String pw = in.next();
+                        
+                        out("\nNome:");
+                        String nome = in.next();
+                              
+                        out("\nEmail:");
+                        String email = in.next();
+                                                    
+                        out("\nMorada:");
+                        String morada = in.next();
+                        
+                        users.put(b,pw);
+                        
+                        if(b > 99999999 && b < 300000000){
+                                registar_individual(b,nome,email,morada,pw);
+                        }
+                        else
+                                registar_empresa(b,nome,email,morada,pw);
+                    }
+                    else{
+                        out("\nNIF já em uso");
+                        break;
+                    }
+                }
+                else {
+                    out("\nNIF inválido");
+                    break;
+                }
+               
+            default:
+                out("\nOpção inválida");
+                break;
+        }
+    }
+   
+ 
 }
