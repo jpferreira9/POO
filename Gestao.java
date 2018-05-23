631d56770a5208b1573e11543606ecd917bc184a
MarkKopfner <28536327+MarkKopfner@users.noreply.github.com>
2018-05-23 04:01:18 +0100

    Update Gestao.java

--- Gestao.java
+++ Gestao.java
@@ -4,6 +4,12 @@ import java.io.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * @author Francisco Correia
+ * @author João Ferreira
+ * @author Luís Félix
+ *
+ */
 public class Gestao implements java.io.Serializable{	
 	
 	private static final long serialVersionUID = 7612055511811556958L;
@@ -14,23 +20,52 @@ public class Gestao implements java.io.Serializable{
     
     Scanner in = new Scanner(System.in);
     
+    /**
+     * Lista de faturas de uma dada empresa
+     */
     ArrayList<Fatura> listaFaturas = new ArrayList<>();
     
+    /**
+     * Mapa que contem users do programa para fins de login (guarda NIF e Password)
+     */
     HashMap<Integer,String> users =  new HashMap<Integer,String>();
+    
+    /**
+     * Mapa que contem NIF e dados da empresa (guarda o NIF como key parte para acesso mais facil)
+     */
     HashMap<Integer,Empresa> dadosEmp = new HashMap<Integer,Empresa>();
+    
+    /**
+     * Mapa que contem NIF e dados de um individuo (guarda o NIF como key parte para acesso mais facil)
+     */
     HashMap<Integer,Individual> dadosInd = new HashMap<Integer, Individual>();
+    
+    /**
+     * Mapa que contem NIF de uma empresa e um arrayList de faturas emitidas pela mesma
+     */
     HashMap<Integer,ArrayList<Fatura>> dadosFat = new HashMap<Integer,ArrayList<Fatura>>();
-      
+    
+    /**
+     * "Substitui" o System.out.println()
+     * @param o objecto dado para imprimir
+     */
     public void out(Object o){
         System.out.println(o.toString());
     }
     
+    /**
+     * Verifica se um NIF é considerado válido segundo os critérios do Portal das Finanças
+     * @param x NIF dado
+     * @return true se o NIF é válido e false caso contrário
+     */
     public boolean nifValido(int x){
         int z = x/100000000;
         if(x == 1234 || (z > 0.9 && z < 10 && (z < 3|| z >= 5 || z < 7 || z >= 8))) return true;
         else return false;
     }
-    
+    /**
+     * Elimina os dados guardados
+     */
     public void limparDados(){
         while(true){
             menu.limpaDados();
@@ -92,27 +127,16 @@ public class Gestao implements java.io.Serializable{
             }
         }
     }
-    
+    /**
+     * Interpreta o menu do administrador
+     */
     public void admin(){
        while(true){ 
             menu.admin();
             switch(in.nextInt()){
                case 1:
 			    // EM PROGRESSO
-                    out("\n10 contribuintes com mais despesas:");
-                    ArrayList<Integer> listaInd = new ArrayList<Integer>();
-                    for(Map.Entry<Integer,Individual> u : dadosInd.entrySet()){
-                		listaInd.add(u.getKey());
-                    }
-                    for(int x=0; x < listaInd.size() ;x++) {
-                    	double total = 0;	
-                    	for(int i=0;i<listaFaturas.size();i++) {
-                    		fat = listaFaturas.get(i);
-                    		if(fat.getNIFCliente()==x) {
-                    			total += fat.getValor();
-                    		}
-                    	}
-                    }
+            	    maisDespesas();
                     break;
                
                case 2:
@@ -139,20 +163,24 @@ public class Gestao implements java.io.Serializable{
             }
         }
     }
-    
+    /**
+     * Interpreta o menu de um utilizador individual
+     * @param x NIF dado
+     */
     public void individual(int x){
         while(true){
             menu.individual(x);
             switch(in.nextInt()){
-                case 1:
-                	//verificar, por parte do contribuinte individual, as despesas que foram emitidas em seu nome 
+                /**
+                 * Verifica, por parte do contribuinte individual, as despesas que foram emitidas em seu nome 
+                 */
+            	case 1:
                     out("\nLista de despesas:");
                     ArrayList<Fatura> fatCliente = new ArrayList<Fatura>();
                     fatCliente = listagemDespesas(x);
                     menu.fatHeader2();
                     for(Fatura f : fatCliente)
                     	menu.impFat2(f.getDEmitente(),f.getNIFEmitente(),f.getData(), f.getDescricao(), f.getValor(), f.getActiv());	
-                    
                     break;
                 /* INCOMPLETO
                 case 2: 
@@ -171,7 +199,10 @@ public class Gestao implements java.io.Serializable{
             }
         }
     }
-    
+    /**
+     * Interpreta o menu de um utilizador Empresa
+     * @param x NIF dado
+     */
     public void empresa(int x){
         while(true){         
             menu.empresa(x);
@@ -185,14 +216,19 @@ public class Gestao implements java.io.Serializable{
                     break;
                 
                 case 2:
-                    out("\nEditar atividade de uma fatura emitida");
+                    out("\nEditar atividade numa fatura emitida");
+                    listaFaturas = dadosFat.get(x);
+                    ArrayList<Fatura> novaListaFats = new ArrayList<Fatura>();
+                    novaListaFats = editar(listaFaturas);
+                    dadosFat.replace(x,novaListaFats);
+                    saveFatura(dadosFat);
                     break;
                     
                 case 3:
                     out("\nLista de Faturas:");
                     listaFaturas = dadosFat.get(x);
                     if(listaFaturas != null) {
-                    	emp.imprimeFaturas(x,listaFaturas);
+                    	dadosEmp.get(x).imprimeFaturas(x,listaFaturas);
                     	break;
                     }
                     else {
@@ -228,9 +264,6 @@ public class Gestao implements java.io.Serializable{
                     break;
                     
                 case 5:
-                    out("\nINTRODUZIR DATA:");
-                    
-                    out("\nTOTAL FATURADO:");
                     totalFaturado(x,listaFaturas);
                     break;
                 
@@ -250,8 +283,10 @@ public class Gestao implements java.io.Serializable{
             }
         }
     }
-    
-    
+    /**
+     * Interpreta o menu das atividades e adiciona a um ArrayList as atividades o utilizador considera relevantes a empresa
+     * @param ativs ArrayList que contem Strings de atividades
+     */
     public void atividades(ArrayList<String> ativs){
         menu.menu_ativs();
         while(true){
@@ -302,7 +337,7 @@ public class Gestao implements java.io.Serializable{
 }
     
     /**
-     * Login e Registo de Entidades
+     * Login e registo de utilizadores
      */
     public Gestao(){
             users.put(1234,"bolas");
@@ -313,7 +348,10 @@ public class Gestao implements java.io.Serializable{
             while(true){    
                 menu.login();                     
                 switch(in.nextInt()){
-                    case 1: // Login de utilizador 
+	                /**
+	                 * Fazer login de um utilizador existente
+	                 */
+                    case 1: 
                         out("\nIntroduza NIF: ");
                         int a = in.nextInt();
                         if(nifValido(a)==true){
@@ -351,8 +389,10 @@ public class Gestao implements java.io.Serializable{
                             out("\nNIF inválido!");
                             break;
                         }
-                                    
-                    case 2: // Registar Utilizador
+                    /**
+                	 * Registar um novo utilizador
+                	 */           
+                    case 2: 
                         out("\nIntroduza NIF: ");
                         a = in.nextInt();
                         if(nifValido(a)==true){
@@ -364,7 +404,10 @@ public class Gestao implements java.io.Serializable{
                                 out("\nIntroduza password:");
                                 String pw = in.next();
                                 users.put(a,pw);
-                                if(tipoI){ // DEFINIDO COMO INDIVIDUAL
+                                /**
+                                 * Utilizador definido como Individual
+                                 */
+                                if(tipoI){ 
                                     out("\nIntroduza o seu nome");
                                     String nom = in.nextLine();
                                     in.nextLine();
@@ -398,7 +441,10 @@ public class Gestao implements java.io.Serializable{
                                     saveIndividual(dadosInd);
                                     
                                 }
-                                else { // DEFINIDO COMO EMPRESA
+                                /**
+                                 * Utilizador definido como Empresa
+                                 */
+                                else { 
                                     out("\nIntroduza o nome da empresa");
                                     String nome = in.nextLine();
                                     nome = in.nextLine();
@@ -442,7 +488,6 @@ public class Gestao implements java.io.Serializable{
      * Gravar os utilizadores criados em ficheiro,
      * para que seja possível retomar mais tarde a execução
      */
-        
     public void saveUsers(HashMap<Integer,String> utilizadores){   
         try{
             File userList = new File("userList");
@@ -463,7 +508,6 @@ public class Gestao implements java.io.Serializable{
     /**
      * Ler os utilizadores em ficheiro
      */
-    
     public void loadUsers(){        
         try{
             File toRead = new File("userList");
@@ -481,9 +525,8 @@ public class Gestao implements java.io.Serializable{
     }
     
     /**
-     * Apagar dados para login de utilizadores
+     * Apagar dados de login de utilizadores
      */
-    
     public void resetUsers(){
         File userList = new File("userList");
         userList.delete();
@@ -492,7 +535,6 @@ public class Gestao implements java.io.Serializable{
     /**
      * Apagar os Faturas guardadas
      */
-    
     public void resetFaturas() {
     	File dadosFat = new File("dadosFat");
     	dadosFat.delete();
@@ -503,7 +545,6 @@ public class Gestao implements java.io.Serializable{
      * Gravar os dados dos NIF's Individuais criados em ficheiro,
      * para que seja possível retomar mais tarde a execução
      */
-        
     public void saveIndividual(HashMap<Integer,Individual> indiv){   
         try{
             File indivList = new File("indivList");
@@ -524,7 +565,6 @@ public class Gestao implements java.io.Serializable{
     /**
      * Ler os dados do Individuais em ficheiro
      */
-    
     public void loadIndividual(){        
         try{
             File toRead = new File("indivList");
@@ -544,7 +584,6 @@ public class Gestao implements java.io.Serializable{
     /**
      * Apagar dados dos Individuais
      */
-    
     public void resetIndiv(){
         File indivList = new File("indivList");
         indivList.delete();
@@ -554,7 +593,6 @@ public class Gestao implements java.io.Serializable{
      * Gravar os dados dos NIF's de Empresas criadas em ficheiro,
      * para que seja possível retomar mais tarde a execução
      */
-        
     public void saveEmpresa(HashMap<Integer,Empresa> empresa){   
         try{
             File empList = new File("empList");
@@ -575,7 +613,6 @@ public class Gestao implements java.io.Serializable{
     /**
      * Ler os dados das Empresas em ficheiro
      */
-    
     public void loadEmpresa(){        
         try{
             File toRead = new File("empList");
@@ -636,18 +673,25 @@ public class Gestao implements java.io.Serializable{
     /**
      * Apagar dados das Empresas
      */
-    
     public void resetEmp(){
         File empList = new File("empList");
         empList.delete();
     }
-    
+    /**
+     * Imprime dados de login de todos os utilizadores registados (NIF e Password) 
+     * @param map mapa de utilizadores
+     */
     public void testPrint(HashMap<Integer,String> map){
         for(Map.Entry<Integer,String> u : map.entrySet()){
                 out("Username: {"+u.getKey()+"} Password: {"+u.getValue()+"}\n");
             }
     }
-
+    
+    /**
+     * Verifica o total faturado por uma empresa x dentro de um intervalo de tempo definido manualmente pelo utilizador
+     * @param x NIF da empresa
+     * @param z	Lista das faturas da empresa
+     */
     public void totalFaturado(int x, ArrayList<Fatura> z) {
     	DateTimeFormatter sdf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     	out("\nIntroduzir data inicial (dd/MM/yyyy):");
@@ -668,7 +712,11 @@ public class Gestao implements java.io.Serializable{
         	}
     	System.out.println("Total faturado pela empresa no intervalo: "+ total + "€");
     }
-    
+    /**
+     * Verifica, por parte do contribuinte individual, as despesas que foram emitidas em seu nome 
+     * @param x NIF de um Individual
+     * @return Lista de faturas em que o NIF do Individual foi definido como cliente
+     */
     public ArrayList<Fatura> listagemDespesas(int x) {
     	ArrayList<Fatura> fatCliente = new ArrayList<Fatura>();
     	for(Map.Entry<Integer,ArrayList<Fatura>> u : dadosFat.entrySet()){
@@ -682,6 +730,76 @@ public class Gestao implements java.io.Serializable{
     	return fatCliente;
     }
     
+    public void maisDespesas() {
+	    out("\n10 contribuintes com mais despesas:");
+	    ArrayList<Fatura> allR = new ArrayList<Fatura>();
+	    ArrayList<Integer> listaNIFS = new ArrayList<Integer>();
+	    ArrayList<Double> listaGASTO = new ArrayList<Double>();
+	    HashMap<Integer,Double> userGasto = new HashMap<Integer,Double>();
+	    for(Entry<Integer, ArrayList<Fatura>> u : dadosFat.entrySet()){
+	    	allR.addAll(u.getValue());
+    	}
+	    for(int i=0;i<allR.size();i++) {
+	    	listaNIFS.add(allR.get(i).getNIFCliente());
+	    	listaGASTO.add(allR.get(i).getValor());
+	    }
+	    for(int i = listaNIFS.size()-1;i>=0;i--) {
+	    	if(listaNIFS.get(i) == listaNIFS.get(i+1)) {
+	    		listaGASTO.set(i, listaGASTO.get(i)+listaGASTO.get(i-1));
+	    		listaNIFS.remove(i);
+	    		listaGASTO.remove(i);
+	    	}
+	    }
+	    /*
+	     * fazer um while verifica se listaNIFS.get(i) existe no resto do array comecando no fim
+	     * se existir retorna o indice em que existe ou int=9999? no caso de nao exisir 
+	     * e remove() /anda array para esq/ o indice depois de adicionar o valor no listaGASTO
+	     * quando nao existir mais, avanca para o indice seguinte ate i=0
+	     * 
+	     */
+	    
+    }
+	    	    
+    /**
+     * 
+     * @param fats
+     * @return
+     */
+    public ArrayList<Fatura> editar(ArrayList<Fatura> fats){
+        menu.clear();
+        out("\n\tIntroduza o numero da fatura que pretende mudar a atividade");
+        int nr = in.nextInt();
+        if(nr>fats.size()){
+            out("\n***** Nao existe fatura com esse numero *****");
+            menu.pausar();
+            return fats;
+        }
+        else{
+            int i = 1;
+            Fatura f = new Fatura();
+            for(Fatura fat: fats){
+                if(i==nr){
+                    f = fat.clone();
+                    fat.setValor(0.0);
+    
+                    break;
+                }
+                i++;
+            }
+            
+            menu.menu_ativs();
+            String novaActiv = "";
+            novaActiv = emp.select_activ(novaActiv);
+            f.setActiv(novaActiv);
+            fats.add(f);
+            menu.clear();
+            out("Atividade modificada com sucesso");
+            menu.pausar();
+        
+            return fats;
+        }
+    }
+
     public static void main(String[] args){        
         new Gestao();
     }
