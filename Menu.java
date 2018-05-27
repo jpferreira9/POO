bf83ceacc371a8100fa530ca08c885400ab98f15
MarkKopfner <28536327+MarkKopfner@users.noreply.github.com>
2018-05-27 16:16:38 +0100

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,36 +1,60 @@
 import java.time.LocalDate;
-public class Menu implements java.io.Serializable{	
-	private static final long serialVersionUID = -5061463634023283339L;
 
+/** JavaFatura
+ * @author Francisco Correia
+ * @author João Ferreira
+ * @author Luís Félix
+ *
+ */
+
+public class Menu implements java.io.Serializable{
+   
+	/**
+	 * Limpa o texto do ecrã
+	 */
 	public void clear() {  
-        System.out.print("\u000C");
+        System.out.print("\u000C");  
         System.out.flush();  
     }
     
+    /**
+     * "Substitui" o System.out.println()
+     * @param o objecto dado para imprimir
+     */
     public void out(Object o){
         System.out.println(o.toString());
     }
     
+    /**
+     * Imprime as opções do menu principal
+     */
     public void login(){
         clear();
-        out("\n\t\t####### Bem vindo ao JavaFactura #######\n\n\n\nEscolha a sua opção:");
+        out("\n\t ####### Bem vindo #######\n\n\n\nEscolha a sua opção: ");
         out("\n\t1) Fazer login"); 
         out("\n\t2) Registar um novo utilizador"); 
+        out("\n\t3) Sair");
     }
     
-    public void individual(int nif){	
+    /**
+     * Imprime as opções do menu user individual
+     */
+    public void individual(int nif){
         clear();
-        out("\n\t ####### Bem vindo #######\n\n\n\nEscolha a sua opção: ");
-        out("\n\t1) Verificar despesas"); 
-        //verificar, por parte do contribuinte individual, as despesas que foram emitidas em seu nome 
-        out("\n\t2) Verificar dedução fiscal acumulada");
-        // verificar o montante de dedução fiscal acumulado, por si e pelo agregado familiar;
-        out("\n\n\t0) Sair");
+        out("\n\t ####### Bem vindo #######\n\n\n\n\tEscolha a sua opção:");
+        out("\n\t1) Ver os seus dados");
+        out("\n\t2) Verificar despesas"); 
+        out("\n\t3) Verificar dedução fiscal acumulada");
+        out("\n\t4) Validar fatura(s)"); 
+        out("\n\n\t0) LogOut");
     }
     
+    /**
+     * Imprime as oções possíveis ao registar uma atividade
+     */
     public void menu_ativs(){
         clear();
-        out("####### Selecione a(s) atividade(s): #######");
+        out("####### Selecione a atividade: #######");
         out("\n\t1) Cabeleireiros");
         out("\t2) Despesas Familiares");
         out("\t3) Educação");
@@ -44,120 +68,139 @@ public class Menu implements java.io.Serializable{
         out("\t11) Veterinários");
         out("\t12) Outros");
         out("\n\t0) Sair");
-}
-     
-    public void empresa(int nif){
-        clear();
-        out("\n\t ####### Bem vindo #######\n\n\n\nEscolha a sua opção: ");
-        out("\n\t1) Criar fatura"); 
-        //criar facturas associadas a despesas feitas por um contribuinte individual
-        out("\n\t2) Editar fatura"); 
-        /* associar/editar classificação de actividade económica a um documento de despesa
-        deve deixar registo para ser depois rastreada */
-        out("\n\t3) Verificar faturas emitidas"); 
-        //obter a listagem das facturas todas de uma determinada empresa, ordenada por data de emissão ou por valor
-        out("\n\t4) Verificar faturas de contribuinte"); 
-        /* obter por parte das empresas, as listagens das facturas por contribuinte num determinado intervalo de datas
-         obter por parte das empresas, as listagens das facturas por contribuinte ordenadas por valor decrescente de despesa */
-        out("\n\t5) Verificar total faturado"); 
-        // indicar o total facturado por uma empresa num determinado período
-        out("\n\t6) Ver lista das atividades da empresa");
-        out("\n\n\t0) Sair");
     }
     
+    /**
+     * Imprime as opções do menu user empresa
+     */
+    public void empresa(int nif){
+        clear();
+        out("\n\t Bem vindo\n\n\n\nEscolha a sua opção:");
+        out("\n\t1) Ver os seus dados");
+        out("\n\t2) Criar fatura"); 
+        out("\n\t3) Verificar faturas emitidas"); 
+        out("\n\t4) Verificar faturas de contribuinte num determinado periodo"); 
+        out("\n\t5) Verificar total faturado num determinado periodo"); 
+        out("\n\t6) Ver lista das atividades da empresa");
+        out("\n\n\t0) LogOut");
+    }
+    
+    /**
+     * Imprime as opções do menu user administrador
+     */
     public void admin(){
         clear();
         out("\n\t Bem vindo ADMIN\n\n\n\nEscolha a sua opção:");
         out("\n\t1) Verificar 10 contribuintes que mais gastam");
-        // determinar a relação dos 10 contribuintes que mais gastam em todo o sistema
         out("\n\t2) Verificar as X empresas com maior nº de faturas e as suas deduções fiscais");
-        /* determinar a relação das X empresas que mais facturas em todo o sistema e o montante de deduções 
-         fiscais que as despesas registadas (dessas empresas) representam
-         empresa: deducao/deducaoTotaldeTodas;
-         */
         out("\n\t3) Ver NIF's registados");
         out("\n\t4) Ver empresas registadas");
         out("\n\t5) Ver individuais registados");
+        out("\n\t6) Ver faturas registados");
         out("\n\t7) Limpar dados");
-        out("\n\n\t0) Sair");
+        out("\n\n\t0) LogOut");
     }
     
+    /**
+     * Imprime as opções do menu user limpa dados
+     */
     public void limpaDados(){
         clear();
         out("\n\t\t\t####### LIMPAR DADOS #######\n\n");
         out("\t1) Eliminar todos os dados");
-        out("\t2) Eliminar dados das Empresas");
-        out("\t3) Eliminar dados Individuais");
+        out("\t2) Eliminar dados das Individuais");
+        out("\t3) Eliminar dados Empresas");
         out("\t4) Eliminar dados Faturas");
         out("\n\t0) Voltar atras");
     }
     
+    /**
+     * Pausa a execuçao do código no tempo dado
+     */
     public void pausar(){
         try {Thread.sleep(2000);} catch (Exception e){}
     }
     
+    /**
+     * Imprime o cabeçalho de uma fatura
+     */
     public void fatHeader() {
-    	out("--------------------------------------------------------------------------------------------------------");
-    	out("      |            |           |             |             |            |            |");
-    	out("  Nº  |  Vendedor  |    NIF    |    Data     | NIF Cliente | Descrição  | Actividade |    Valor  ");
-    	out("      |            |           |             |             |            |            |");
-    	out("--------------------------------------------------------------------------------------------------------");
+        out("-----------------------------------------------------------------------------------------------------------------------------");
+        out("      |            |           |             |             |            |            |                |");
+        out("  Nº  |  Vendedor  |    NIF    |    Data     | NIF Cliente | Descrição  | Actividade |  Taxa deducao  |  Valor  ");
+        out("      |            |           |             |             |            |            |                |");
+        out("-----------------------------------------------------------------------------------------------------------------------------");
     }
     
-    public void impFat(int numFat, int nifE, String nome, LocalDate dataF, int nifC, String desc, double v, String activ) {
-    	String numFatF = String.format("%04d", numFat);
-    	String nomeF = nome.substring(0, Math.min(nome.length(), 10));
-    	String descF = desc.substring(0, Math.min(desc.length(), 10));
-    	String activF = activ.substring(0, Math.min(activ.length(), 10));
-    	while(nomeF.length()<10) {
-    		nomeF += " ";
-    	}
-    	while(descF.length()<10) {
-    		descF += " ";
-    	}
-    	while(activF.length()<10) {
-    		activF += " ";
-    	}
-    	out(numFatF + "  | " +nomeF+ " | " +nifE+ " |  " +dataF+" | " +nifC+ "   | " +descF+ " | " +activF+ " | " +v+"€ ");
-    	out("--------------------------------------------------------------------------------------------------------");
+    /**
+     * Imprime os dados de uma fatura
+     */    
+    public void impFat(int numFat, int nifE, String nome, LocalDate dataF, int nifC, String desc, double v, String activ, double deducao) {
+        String numFatF = String.format("%04d", numFat);
+        String nomeF = nome.substring(0, Math.min(nome.length(), 10));
+        String descF = desc.substring(0, Math.min(desc.length(), 10));
+        String activF = activ.substring(0, Math.min(activ.length(), 10));
+        while(nomeF.length()<10) {
+            nomeF += " ";
+        }
+        while(descF.length()<10) {
+            descF += " ";
+        }
+        while(activF.length()<10) {
+            activF += " ";
+        }
+        out(numFatF + "  | " +nomeF+ " | " +nifE+ " |  " +dataF+" | " +nifC+ "   | " +descF+ " | " +activF+ " |     "+(deducao*100)+"%      |   "  +v+"€ ");
+        out("-----------------------------------------------------------------------------------------------------------------------------");
     }
     
+    /**
+     * Imprime o cabeçalho da lista de despesas de um user individual
+     */
     public void fatHeader2() {
-    	out("--------------------------------------------------------------------------------------------------------");
-    	out("            |           |             |            |            |");
-    	out("  Vendedor  |    NIF    |    Data     | Descrição  | Actividade |    Valor  ");
-    	out("            |           |             |            |            |");
-    	out("--------------------------------------------------------------------------------------------------------");
+        out("-------------------------------------------------------------------------------------");
+        out("            |           |             |            |            |               ");
+        out("  Vendedor  |    NIF    |    Data     | Descrição  | Actividade |   Valor  ");
+        out("            |           |             |            |            |               ");
+        out("-------------------------------------------------------------------------------------");
     }
+    
+    /**
+     * Imprime os dados da lista de despesas de um user individual
+     */
     public void impFat2(String nomeVendedor, int nifVendedor, LocalDate dataF, String desc, double v, String activ) {
-    	String nomeF = nomeVendedor.substring(0, Math.min(nomeVendedor.length(), 10));
-    	String descF = desc.substring(0, Math.min(desc.length(), 10));
-    	String activF = activ.substring(0, Math.min(activ.length(), 10));
-    	while(nomeF.length()<10) {
-    		nomeF += " ";
-    	}
-    	while(descF.length()<10) {
-    		descF += " ";
-    	}
-    	while(activF.length()<10) {
-    		activF += " ";
-    	}
-    	out(nomeF+ " | " +nifVendedor+ " |  " +dataF+" | " +descF+ " | " +activF+ " | " +v+"€ ");
-    	out("--------------------------------------------------------------------------------------------------------");
+        String nomeF = nomeVendedor.substring(0, Math.min(nomeVendedor.length(), 10));
+        String descF = desc.substring(0, Math.min(desc.length(), 10));
+        String activF = activ.substring(0, Math.min(activ.length(), 10));
+        while(nomeF.length()<10) {
+            nomeF += " ";
+        }
+        while(descF.length()<10) {
+            descF += " ";
+        }
+        while(activF.length()<10) {
+            activF += " ";
+        }
+        out(nomeF+ "  | " +nifVendedor+ " |  " +dataF+" | " +descF+ " | " +activF+ " |  " +v+"€ ");
+        out("-------------------------------------------------------------------------------------");
     }
     
+    /**
+     * Imprime o cabeçalho da lista de despesas e respetiva dedução
+     */
     public void fatHeader3() {
-        out("------------------------------------------------------------------------------------");
+        out("------------------------------------------------------------------------------");
         out("      |            |           |");
-        out("  Nº  |  Vendedor  |    NIF    |    Valor deduzível / Valor Total  ");
+        out("  Nº  |  Vendedor  |    NIF    |  Valor deduzivel / Valor Total  ");
         out("      |            |           |");
-        out("------------------------------------------------------------------------------------");
+        out("-------------------------------------------------------------------------------");
     }
     
+    /**
+     * Imprime mensagem de despedida da aplicação
+     */
     public void sair(){
         clear();
-        out("\n\n\tObrigado pela visita");
+        out("\n\n\tObrigado pela sua preferencia.");
     }
     
 }
-
